6cf215215fadedef6cca797685bd9d6af4620584
Anuda Weerasinghe <anuda@live.com>
2023-10-05 18:09:33 -0400

    fixes config

--- create_config.sh
+++ create_config.sh
@@ -18,11 +18,19 @@ if [[ -z "${REDIS_PASSWORD+x}" ]]; then
   exit 1
 fi
 
+if [[ -z "${DEPLOYMENT_URL+x}" ]]; then
+  # var is not defined
+  echo "Error: DEPLOYMENT_URL is not defined!"
+  exit 1
+fi
+
 # Read the JSON file
 json_data=$(cat "config_template.json")
 
 # Update the JSON file with the environment variables
-json_data=$(jq --arg host "$REDIS_HOST" --arg port "$REDIS_PORT" --arg password "$REDIS_PASSWORD" '.redis.host = $host | .redis.port = $port | .redis.password = $password' <<< "$json_data")
+json_data=$(jq --arg host "$REDIS_HOST" --arg port "$REDIS_PORT" --arg password "$REDIS_PASSWORD" --arg url "$DEPLOYMENT_URL" '.url = $url | .redis.host = $host | .redis.port = $port | .redis.password = $password' <<< "$json_data")
 
 # Write the updated JSON file to config.json
 echo "$json_data" > "config.json"
+
+cat config.json
