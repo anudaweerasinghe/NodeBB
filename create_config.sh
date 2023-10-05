7529f8aaf8a10fe3864470137cc6efe705601523
Anuda Weerasinghe <anuda@live.com>
2023-10-05 14:17:43 -0400

    adds checks for env vars

--- create_config.sh
+++ create_config.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+if [[ -z "${REDIS_HOST+x}" ]]; then
+  # var is not defined
+  echo "Error: REDIS_HOST is not defined!"
+  exit 1
+fi
+
+if [[ -z "${REDIS_PORT+x}" ]]; then
+  # var is not defined
+  echo "Error: REDIS_PORT is not defined!"
+  exit 1
+fi
+
+if [[ -z "${REDIS_PASSWORD+x}" ]]; then
+  # var is not defined
+  echo "Error: REDIS_PASSWORD is not defined!"
+  exit 1
+fi
+
 # Read the JSON file
 json_data=$(cat "config_template.json")
 
@@ -8,5 +26,3 @@ json_data=$(jq --arg host "$REDIS_HOST" --arg port "$REDIS_PORT" --arg password 
 
 # Write the updated JSON file to config.json
 echo "$json_data" > "config.json"
-
-cat config.json
