7f48c6212987be1fd6f584c340855c17473136ed
Anuda Weerasinghe <anuda@live.com>
2023-10-05 18:28:52 -0400

    fixes config

--- create_config.sh
+++ create_config.sh
@@ -28,7 +28,7 @@ fi
 json_data=$(cat "config_template.json")
 
 # Update the JSON file with the environment variables
-json_data=$(jq  --arg url "$DEPLOYMENT_URL" --arg host "$REDIS_HOST" --arg port "$REDIS_PORT" --arg password "$REDIS_PASSWORD" '.url = $url | .redis.host = $host | .redis.port = $port | .redis.password = $password' <<< "$json_data")
+json_data=$(jq --arg url "$DEPLOYMENT_URL" --arg host "$REDIS_HOST" --arg port "$REDIS_PORT" --arg password "$REDIS_PASSWORD" '.url = $url | .redis.host = $host | .redis.port = $port | .redis.password = $password' <<< "$json_data")
 
 # Write the updated JSON file to config.json
 echo "$json_data" > "config.json"
